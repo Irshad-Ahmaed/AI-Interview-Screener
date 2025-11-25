4910a4bad968061283e3249bd758d3d61c7877c2
IRSHAD AHMAD <129326052+Irshad-Ahmaed@users.noreply.github.com>
2025-11-25 10:19:44 +0530

    Validation rank answer value updated

--- src/validators/rank.validator.js
+++ src/validators/rank.validator.js
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const RankValidator = z.object({
-  answers: z.array(z.string().min(1)).min(10, {
-    message: "You must provide at least 10 answers to rank.",
+  answers: z.array(z.string().min(1)).min(2, {
+    message: "You must provide at least 2 answers to rank.",
   })
 });
